6739e0a54330477fd8eea7ef6fc06d902d32b07b
PeterC <petercalifano.gs@gmail.com>
2024-01-03 17:49:05 +0100

    Added printing of units

--- scripts/logIMUdata.py
+++ scripts/logIMUdata.py
@@ -27,7 +27,7 @@ LOGGILE_NAME = 'BN055_IMUdata_LOG'
 
 LOGFILE_PATH = os.path.join(LOGFILE_DIR, LOGGILE_NAME)
 
-def readIMUData_BN055(BN055sensor, IMUdata):
+def readIMUData_BN055(BN055sensor, IMUdata, IMUdataUnits):
 
     # Read data from BN055sensor instance
     (eulerX, eulerY, eulerZ) = BN055sensor.euler
@@ -35,31 +35,51 @@ def readIMUData_BN055(BN055sensor, IMUdata):
     IMUdata["eulerY"] = eulerY
     IMUdata["eulerZ"] = eulerZ
 
+    IMUdataUnits["eulerX"] = 'deg' 
+    IMUdataUnits["eulerY"] = 'deg'
+    IMUdataUnits["eulerZ"] = 'deg' 
+
     (lin_accelX, lin_accelY, lin_accelZ) = BN055sensor.linear_acceleration
     IMUdata["lin_accelX"] = lin_accelX
     IMUdata["lin_accelY"] = lin_accelY
     IMUdata["lin_accelZ"] = lin_accelZ
+
+    IMUdataUnits["lin_accelX"] = 'm/s^2' 
+    IMUdataUnits["lin_accelY"] = 'm/s^2'
+    IMUdataUnits["lin_accelZ"] = 'm/s^2'
 
     (accelX, accelY, accelZ) = BN055sensor.acceleration
     IMUdata["accelX"] = accelX
     IMUdata["accelY"] = accelY
     IMUdata["accelZ"] = accelZ
 
+    IMUdataUnits["accelX"] = 'm/s^2' 
+    IMUdataUnits["accelY"] = 'm/s^2'
+    IMUdataUnits["accelZ"] = 'm/s^2'
+
     (magX, magY, magZ) = BN055sensor.magnetic
     IMUdata["magX"] = magX
     IMUdata["magY"] = magY
     IMUdata["magZ"] = magZ
+
+    IMUdataUnits["magX"] = 'muT' 
+    IMUdataUnits["magY"] = 'muT'
+    IMUdataUnits["magZ"] = 'muT'
 
     (gyrosX, gyrosY, gyrosZ) = BN055sensor.gyro
     IMUdata["gyrosX"] = gyrosX
     IMUdata["gyrosY"] = gyrosY
     IMUdata["gyrosZ"] = gyrosZ
 
+    IMUdataUnits["gyrosX"] = 'rad/s' 
+    IMUdataUnits["gyrosY"] = 'rad/s'
+    IMUdataUnits["gyrosZ"] = 'rad/s'
+
     #IMUdata["temp"] = BN055sensor.temperature
     #IMUdata["calibration"] = BN055sensor.calibration_status
     return IMUdata
 
-def writeIMUData_BN055(IMUdata, LOGFILE_PATH):
+def writeIMUData_BN055(IMUdata, LOGFILE_PATH, IMUdataUnits={}):
     FILE_JUST_CREATED=False
     # Check if file exists
     if not(os.path.exists(LOGFILE_PATH)):
@@ -77,6 +97,8 @@ def writeIMUData_BN055(IMUdata, LOGFILE_PATH):
         # Write headers
         headers = IMUdata.keys()
         csvFileWriter.writerow(headers) # If empty write headers
+        if len(IMUdataUnits.keys()) == len(headers):
+            csvFileWriter.writerow(IMUdataUnits.values()) # Write units
 
     values = IMUdata.values() # Convert dictionary to lists
     csvFileWriter.writerow(values) # Write data to file
@@ -88,6 +110,7 @@ def writeIMUData_BN055(IMUdata, LOGFILE_PATH):
 
 def main():   
     IMUdata = {} # Data dictionary
+    IMUdataUnits = {} # Data units dictionary
     elapsed_time = 0.0 # [s]
     SleepTime = 1.0 / BNO_UPDATE_FREQUENCY_HZ
 
@@ -103,11 +126,11 @@ def main():
     while elapsed_time <= LOG_TIME:
         IMUdata['Timetag'] = startTime + elapsed_time
         # Read and Write data to dictionary
-        IMUdata = readIMUData_BN055(BN055sensor, IMUdata)
+        (IMUdata, IMUdataUnits) = readIMUData_BN055(BN055sensor, IMUdata, IMUdataUnits)
 
        # Time writing to file routine and subtract to SleepTime (rough method)
         timeInit = time.perf_counter()
-        writeIMUData_BN055(IMUdata, LOGFILE_PATH)
+        writeIMUData_BN055(IMUdata, LOGFILE_PATH, IMUdataUnits)
         writingTime = time.perf_counter() - timeInit
 
         # Wait time to have (roughly) the desired reading frequency
